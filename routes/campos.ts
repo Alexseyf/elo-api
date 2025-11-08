800c3b0b88c15d4fe4e934ceafa442bf0d76dbd8
Alexandre Seyffert <alexseyf66@gmail.com>
2025-11-08 14:24:07 -0300

    add 'campos_de_experiencia' table

---
+++ routes/campos.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, CAMPO_EXPERIENCIA } from "@prisma/client";
+import { Router} from "express";
+import { z} from "zod";
+import { checkToken } from "../middlewares/checkToken";
+import { checkRoles } from "../middlewares/checkRoles";
+
+const prisma = new PrismaClient();
+const router = Router();
+
+const campoSchema = z.object({
+  campoExperiencia: z.nativeEnum(CAMPO_EXPERIENCIA)
+})
+
+router.post("/campos", checkToken, checkRoles(["ADMIN"]), async (req, res) => {
+    const valida = campoSchema.safeParse(req.body);
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error });
+        return;
+    }
+
+    try {
+        const campo = await prisma.camposDeExperiencia.create({
+            data: valida.data
+        });
+        res.status(201).json(campo);
+    } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+router.get("/campos", checkToken, async (req, res) => {
+    try {
+        const campos = await prisma.camposDeExperiencia.findMany({
+            orderBy: {
+                id: 'asc'
+            }
+        });
+        res.status(200).json(campos);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+export { router };
